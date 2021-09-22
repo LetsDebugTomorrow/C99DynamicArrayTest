88acfacc8a6bfcd9cd8d08fe19cf844749460197
TangHaomiao <1020145095@qq.com>
2021-09-22 19:29:02 +0800

    finished

---
+++ cmake-build-debug/CMakeFiles/DynamicArrayTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DynamicArrayTest.dir/main.c.o"
+  "DynamicArrayTest"
+  "DynamicArrayTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DynamicArrayTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
